ec0090a1431829e7b9b5d558e730ca228a8ee747
Michel Moraes <michel@nitdesign.com.br>
2021-06-10 17:24:01 +0200

    adding hero field conditional

--- partials/hero.php
+++ partials/hero.php
@@ -9,15 +9,17 @@
     <div class="hero__content text-center">
         <img src="<?php echo $hero_logo['url'] ?>" alt="<?php echo $hero_logo['alt'] ?>" class="hero__logo">
         <h2 class="hero__subtitle"><?php echo $hero_subtitle ?></h2>
-        <ul class="plataforms list-unstyled d-flex justify-content-center align-items-center">
-            <?php foreach ($platforms as $platform) : ?>
-                <li>
-                    <img 
-                        src="<?php echo $platform['logo']['url'] ?>" 
-                        alt="<?php echo $platform['logo']['alt'] ?>" 
-                        class="platforms__logo">
-                </li>  
-            <?php endforeach; ?>
-        </ul>
+        <?php if ( ! empty ( $platforms ) ) : ?>
+            <ul class="plataforms list-unstyled d-flex justify-content-center align-items-center">
+                <?php foreach ($platforms as $platform) : ?>
+                    <li>
+                        <img 
+                            src="<?php echo $platform['logo']['url'] ?>" 
+                            alt="<?php echo $platform['logo']['alt'] ?>" 
+                            class="platforms__logo">
+                    </li>  
+                <?php endforeach; ?>
+            </ul>
+        <?php endif ?>
     </div>
 </section>
